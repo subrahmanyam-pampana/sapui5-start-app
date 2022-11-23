d500b1d8e31a671488583b9d1a8b814d4bf18dcf
sp <subrahmanyam.pampana.28@gmail.com>
2022-11-23 17:43:42 +0530

    removed the json modal

--- webapp/Component.js
+++ webapp/Component.js
@@ -14,15 +14,6 @@ sap.ui.define([
 		init : function () {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
-
-			// set data model
-			var oData = {
-				recipient : {
-					name : "World"
-				}
-			};
-			var oModel = new JSONModel(oData);
-			this.setModel(oModel);
 		}
 	});
 
